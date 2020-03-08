c708d355ae312f3f2699956fe216cf20bb1e6a33
Valckyz <Valckyz@github.com>
2020-03-08 19:15:34 -0400

    Status Change

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Client, Collection } = require("discord.js");
 const { config } = require("dotenv");
 const TOKEN = process.env.TOKEN;
+let statuses = [`${client.guilds.size} | /help`, `Version 1.2 | /help`];
 
 const client = new Client({
     disableEveryone: false
@@ -22,13 +23,20 @@ config({
 client.on("ready", () => {
     console.log(`Hi, ${client.user.username} is now online!, No Crashes Detected`);
 
-    client.user.setPresence({
-        status: "online",
-        game: {
-            name:  `${client.guilds.size} Servers |  /help`,
-            type: "WATCHING"
-        }
-    }); 
+    setInterval(function() {
+
+
+        let status = statuses[Math.floor(Math.random()*statuses.length)];
+
+
+        client.user.setPresence({ 
+            game: {
+                name: status,
+                type: "Watching",
+            }
+        });
+
+    }, 7000)
 })
 
 client.on("message", async message => {
