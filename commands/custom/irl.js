b0127e7f6b710a0b988e2c7d9eb97d373c278dc9
Valckyz <Valckyz@github.com>
2020-03-21 16:38:23 -0400

    NSFW Commands

---
+++ commands/custom/irl.js
@@ -0,0 +1,38 @@
+const { RichEmbed } = require("discord.js");
+const Discord = require("discord.js");
+const randomPuppy = require("random-puppy");
+
+module.exports = {
+    name: "irl",
+    aliases: [""],
+    category: "fun",
+    description: "Sends an epic cheese",
+    run: async (client, message, args) => {
+        message.delete()
+        // In this array, 
+        // you can put the subreddits you want to grab memes from
+        const subReddits = ["IRLGirls"];
+        // Grab a random property from the array
+        const random = subReddits[Math.floor(Math.random() * subReddits.length)];
+
+        let ambed = new Discord.RichEmbed()
+        .setDescription("🔒 This is not a NSFW channel")
+        .setFooter("psst! make sure this is a NSFW channel!")
+
+
+        // Get a random image from the subreddit page
+        const img = await randomPuppy(random);
+        const embed = new RichEmbed()
+            .setColor("RANDOM")
+            .setImage(img)
+            .setTitle(`From /r/${random}`)
+            .setURL(`https://reddit.com/r/${random}`);
+
+        if (message.channel.nsfw === true) {
+            message.channel.send(embed);
+        } else {
+            message.channel.send(ambed).then(m => m.delete(5000));
+
+            console.log(bot.guilds.forEach())
+    }
+    }}
---
+++ commands/custom/stars.js
@@ -0,0 +1,36 @@
+const { RichEmbed } = require("discord.js");
+const Discord = require("discord.js");
+const randomPuppy = require("random-puppy");
+
+module.exports = {
+    name: "stars",
+    aliases: [""],
+    category: "fun",
+    description: "Sends an epic cheese",
+    run: async (client, message, args) => {
+        message.delete()
+        // In this array, 
+        // you can put the subreddits you want to grab memes from
+        const subReddits = ["InternetStars"];
+        // Grab a random property from the array
+        const random = subReddits[Math.floor(Math.random() * subReddits.length)];
+
+        let ambed = new Discord.RichEmbed()
+        .setDescription("🔒 This is not a NSFW channel")
+        .setFooter("psst! make sure this is a NSFW channel!")
+
+
+        // Get a random image from the subreddit page
+        const img = await randomPuppy(random);
+        const embed = new RichEmbed()
+            .setColor("RANDOM")
+            .setImage(img)
+            .setTitle(`From /r/${random}`)
+            .setURL(`https://reddit.com/r/${random}`);
+
+        if (message.channel.nsfw === true) {
+            message.channel.send(embed);
+        } else {
+            message.channel.send(ambed).then(m => m.delete(5000));
+    }
+    }}
